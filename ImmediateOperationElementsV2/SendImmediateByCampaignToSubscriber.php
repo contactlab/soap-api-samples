ebf9e70a63a960f25c891db87ec194ede3f0b81c
Marco Zanzottera <marco.zanzottera@contactlab.com>
2015-11-13 15:02:48 +0100

    small fix

--- ImmediateOperationElementsV2/SendImmediateByCampaignToSubscriber.php
+++ ImmediateOperationElementsV2/SendImmediateByCampaignToSubscriber.php
@@ -1,207 +1,162 @@
 <?php
 
+require __DIR__.'/../vendor/autoload.php';
 
-    require __DIR__.'/../vendor/autoload.php';
+// REQUIRED
 
-    /*  Start Mandatory   */
+$apiKey = <<api key >>;
+$userKey = <<user key >>;
 
-    $apiKey = <<api key >>;
-    $userKey = <<user key >>;
+$recipient = 'existing.email@contactlab.com';
+$campaignId = <<id campaign>>;
 
-    $recipient = 'existing.email@contactlab.com';
-    $campaignId = <<id campaign>>;
+// Optional
 
-    /*  End Mandatory   */
+$nameRecipient = 'Mario';
 
-    /*  Start Optional  */
+$senderName = 'ContactLab';
+$senderEmail = 'noreply@contactlab.com';
+$senderReplyTo = 'noreply@contactlab.com';
 
-    $nameRecipient = 'Mario';
+$subject = 'TEST - email subject';
 
-    $senderName = 'ContactLab';
-    $senderEmail = 'noreply@contactlab.com';
-    $senderReplyTo = 'noreply@contactlab.com';
+$htmlContent = '<b>HTML CONTENT ${nameRecipient}$</b>';
+$textContent = 'Alternative Test ${nameRecipient}$';
 
-    $subject = 'TEST - email subject';
+$bccRecipients = array('bcc.email1@contactlab.com','bcc.email2@contactlab.com');
+$ccRecipients = array('cc.email1@contactlab.com','cc.email2@contactlab.com');
+$includeDefaultAttachments = false;
 
-    $htmlContent = '<b>HTML CONTENT ${nameRecipient}$</b>';
-    $textContent = 'Alternative Test ${nameRecipient}$';
+$attachmentName = 'change_name.txt';
+$attachmentContent = 'change_content';
+$mimeType = MimeType::TXT;
 
-    $bccRecipients = array('bcc.email1@contactlab.com','bcc.email2@contactlab.com');
-    $ccRecipients = array('cc.email1@contactlab.com','cc.email2@contactlab.com');
-    $includeDefaultAttachments = false;
+/**
+ * Soap Service
+ */
+$clabService = new ClabService(
+    array(
+        'soap_version' => SOAP_1_2,
+        'connection_timeout' => 30,
+        'trace' => true,
+        'keep_alive' => true,
+    )
+);
 
-    $attachmentName = 'change_name.txt';
-    $attachmentContent = 'change_content';
-    $mimeType = MimeType::TXT;
+/**
+ * Preparing subscriber
+ */
+$userData = new Subscriber();
 
-    /*  End Optional   */
+$attributes = array();
 
-    /*  Soap Service */
+$attribute = new SubscriberAttribute();
+// "RECIPIENT" is a mandatory attribute for sending an email using sendImmediateMessages
+$attribute->key = 'RECIPIENT';
+// SoapVar is necessary for SubscriberAttribute because every attribute can have different type from others
+$attribute->value = new SoapVar($recipient, XSD_ANYTYPE, 'string', 'http://www.w3.org/2001/XMLSchema', 'value');
+$attributes[] = $attribute;
 
-    $clabService = new ClabService(
-        array(
-            'soap_version' => SOAP_1_2,
-            'connection_timeout' => 30,
-            'trace' => true,
-            'keep_alive' => true,
-        )
-    );
-
-    /*
-     * Object Subscriber
-     * @var int $identifier
-     * @var SubscriberAttribute[] $attributes
-    */
-
-    $userData = new Subscriber();
-
-    $attributes = array();
-
-    /*
-     *  SoapVar is necessary for SubscriberAttribute because
-     *  every attribute can have different type from others
-    */
-
-    /*
-     * "RECIPIENT" is a mandatory attribute for sending an email using sendImmediateMessages
-    */
-
+if (isset($nameRecipient)) {
     $attribute = new SubscriberAttribute();
-    $attribute->key = 'RECIPIENT';
-    $attribute->value = new SoapVar($recipient, XSD_ANYTYPE, 'string', 'http://www.w3.org/2001/XMLSchema', 'value');
+    $attribute->key = 'nameRecipient';
+    $attribute->value = new SoapVar($nameRecipient, XSD_ANYTYPE, 'string', 'http://www.w3.org/2001/XMLSchema', 'value');
     $attributes[] = $attribute;
+}
+$userData->attributes = $attributes;
 
-    if (isset($nameRecipient)) {
-        $attribute = new SubscriberAttribute();
-        $attribute->key = 'nameRecipient';
-        $attribute->value = new SoapVar($nameRecipient, XSD_ANYTYPE, 'string', 'http://www.w3.org/2001/XMLSchema', 'value');
-        $attributes[] = $attribute;
+/**
+ * Preparing campaign
+ */
+
+// borrowToken
+$borrowTokenParameters = new borrowToken();
+$borrowTokenParameters->apiKey = $apiKey;
+$borrowTokenParameters->userKey = $userKey;
+$token = $clabService->borrowToken($borrowTokenParameters)->return;
+
+// getCampaign
+$getCampaign = new getCampaign();
+$getCampaign->token = $token;
+$getCampaign->campaignIdentifier = $campaignId;
+$campaign = $clabService->getCampaign($getCampaign)->return;
+
+// Invalidate Token because sendImmediate methods don't need token
+$invalidateTokenParameters = new invalidateToken();
+$invalidateTokenParameters->token = $token;
+$clabService->invalidateToken($invalidateTokenParameters);
+
+/**
+ * Optional customization
+ */
+if (isset($senderName)) {
+    $campaign->message->sender->name = $senderName;
+}
+if (isset($senderEmail)) {
+    $campaign->message->sender->email = $senderEmail;
+}
+if (isset($senderReplyTo)) {
+    $campaign->message->sender->replyTo = $senderReplyTo;
+}
+if (isset($subject)) {
+    $campaign->message->subject = $subject;
+}
+if (isset($htmlContent)) {
+    $campaign->message->htmlContent = $htmlContent;
+}
+if (isset($textContent)) {
+    $campaign->message->textContent = $textContent;
+}
+
+// SoapVar is necessary for Message because every message can be EmailMessage, TextMessage, FaxMessage or PushMessage
+$campaign->message = new \SoapVar($campaign->message, SOAP_ENC_OBJECT, 'EmailMessage', 'domain.ws.api.contactlab.com');
+
+/**
+ * Preparing sendImmadiateOption
+ */
+$sendImmediateOptions = new SendImmediateOptions();
+
+if (isset($bccRecipients)) {
+    $sendImmediateOptions->bccRecipients = $bccRecipients;
+}
+
+if (isset($ccRecipients)) {
+    $sendImmediateOptions->ccRecipients = $ccRecipients;
+}
+
+$attachments = array();
+if (isset($attachmentName)) {
+    $attachment = new Attachment();
+    $attachment->campaignIdentifier = $campaignId;
+    $attachment->name = $attachmentName;
+    $attachment->content = $attachmentContent;
+    $attachment->mimeType = MimeType::BINARY;
+    if (isset($mimeType)) {
+        $attachment->mimeType = $mimeType;
     }
-    $userData->attributes = $attributes;
+    $attachments[] = $attachment;
+}
 
-    /*
-     * Object borrowToken
-     * @var string $apiKey
-     * @var string $userKey
-     * @return AuthToken $token (in 'return' key)
-    */
+$sendImmediateOptions->customAttachments = $attachments;
 
-    $borrowTokenParameters = new borrowToken();
+if (isset($includeDefaultAttachments)) {
+    $sendImmediateOptions->includeDefaultAttachments = $includeDefaultAttachments;
+}
 
-    $borrowTokenParameters->apiKey = $apiKey;
-    $borrowTokenParameters->userKey = $userKey;
+/**
+ * Object sendImmediateByCampaignToSubscriber
+ * @var string $apiKey
+ * @var string $userKey
+ * @var Campaign $campaign
+ * @var Subscriber $userData
+ * @var SendImmediateOptions $sendImmediateOptions
+ */
+$sendImmediateByCampaignToSubscriberParameters = new sendImmediateByCampaignToSubscriber();
 
-    $token = $clabService->borrowToken($borrowTokenParameters)->return;
+$sendImmediateByCampaignToSubscriberParameters->apiKey = $apiKey;
+$sendImmediateByCampaignToSubscriberParameters->userKey = $userKey;
+$sendImmediateByCampaignToSubscriberParameters->campaign = $campaign;
+$sendImmediateByCampaignToSubscriberParameters->subscriber = $userData;
+$sendImmediateByCampaignToSubscriberParameters->sendImmediateOptions = $sendImmediateOptions;
 
-    /*
-     * Object getCampaign
-     * @var AuthToken $token
-     * @var int $campaignIdentifier
-     * @return Campaign $campaign (in 'return' key)
-    */
-
-    $getCampaign = new getCampaign();
-
-    $getCampaign->token = $token;
-
-    $getCampaign->campaignIdentifier = $campaignId;
-
-    $campaign = $clabService->getCampaign($getCampaign)->return;
-
-    if (isset($senderName)) {
-        $campaign->message->sender->name = $senderName;
-    }
-    if (isset($senderEmail)) {
-        $campaign->message->sender->email = $senderEmail;
-    }
-    if (isset($senderReplyTo)) {
-        $campaign->message->sender->replyTo = $senderReplyTo;
-    }
-
-    if (isset($subject)) {
-        $campaign->message->subject = $subject;
-    }
-
-    if (isset($htmlContent)) {
-        $campaign->message->htmlContent = $htmlContent;
-    }
-
-    if (isset($textContent)) {
-        $campaign->message->textContent = $textContent;
-    }
-
-    /*
-     *  SoapVar is necessary for Message because
-     *  every message can be EmailMessage, TextMessage, FaxMessage or PushMessage
-    */
-
-    $campaign->message = new \SoapVar($campaign->message, SOAP_ENC_OBJECT, 'EmailMessage', 'domain.ws.api.contactlab.com');
-
-    $attachments = array();
-    if (isset($attachmentName)) {
-        $attachment = new Attachment();
-        $attachment->campaignIdentifier = $campaignId;
-        $attachment->name = $attachmentName;
-        $attachment->content = $attachmentContent;
-        $attachment->mimeType = MimeType::BINARY;
-        if (isset($mimeType)) {
-            $attachment->mimeType = $mimeType;
-        }
-        $attachments[] = $attachment;
-    }
-
-    /*
-     *  Invalidate Token because sendImmediate methods don't need token
-    */
-
-    /*
-     * Object invalidateToken
-     * @var AuthToken $token
-    */
-
-    $invalidateTokenParameters = new invalidateToken();
-    $invalidateTokenParameters->token = $token;
-
-    $clabService->invalidateToken($invalidateTokenParameters);
-
-    /*
-     * Object SendImmediateOptions
-     * @var string[] $bccRecipients
-     * @var string[] $ccRecipients
-     * @var Attachment[] $attachments
-     * @var boolean $includeDefaultAttachments
-    */
-
-    $sendImmediateOptions = new SendImmediateOptions();
-
-    if (isset($bccRecipients)) {
-        $sendImmediateOptions->bccRecipients = $bccRecipients;
-    }
-
-    if (isset($ccRecipients)) {
-        $sendImmediateOptions->ccRecipients = $ccRecipients;
-    }
-
-    $sendImmediateOptions->customAttachments = $attachments;
-
-    if (isset($includeDefaultAttachments)) {
-        $sendImmediateOptions->includeDefaultAttachments = $includeDefaultAttachments;
-    }
-
-    /*
-     * Object sendImmediateByCampaignToSubscriber
-     * @var string $apiKey
-     * @var string $userKey
-     * @var Campaign $campaign
-     * @var Subscriber $userData
-     * @var SendImmediateOptions $sendImmediateOptions
-    */
-    $sendImmediateByCampaignToSubscriberParameters = new sendImmediateByCampaignToSubscriber();
-
-    $sendImmediateByCampaignToSubscriberParameters->apiKey = $apiKey;
-    $sendImmediateByCampaignToSubscriberParameters->userKey = $userKey;
-    $sendImmediateByCampaignToSubscriberParameters->campaign = $campaign;
-    $sendImmediateByCampaignToSubscriberParameters->subscriber = $userData;
-    $sendImmediateByCampaignToSubscriberParameters->sendImmediateOptions = $sendImmediateOptions;
-
-    $uuid = $clabService->sendImmediateByCampaignToSubscriber($sendImmediateByCampaignToSubscriberParameters)->return;
+$uuid = $clabService->sendImmediateByCampaignToSubscriber($sendImmediateByCampaignToSubscriberParameters)->return;
