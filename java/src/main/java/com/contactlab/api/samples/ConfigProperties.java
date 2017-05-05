d841d0999bd454b5901b96506d9b85ed9b9557bb
Sabrina D'Accorso <sabrina.daccorso@contactlab.com>
2017-05-05 10:59:28 +0200

    chnage config props

--- java/src/main/java/com/contactlab/api/samples/ConfigProperties.java
+++ java/src/main/java/com/contactlab/api/samples/ConfigProperties.java
@@ -3,7 +3,7 @@ package com.contactlab.api.samples;
 public class ConfigProperties {
 
     /* Data for WS authentication */
-    public static String API_KEY = "99999999df0ee7d52389ae332a2400c29ca20f6f";
-    public static String USER_KEY = "00000000c4c6cde9d5492d2f8f083d70c040d549";
+    public static String API_KEY = "<api-key>";
+    public static String USER_KEY = "<user-key>";
 
 }
